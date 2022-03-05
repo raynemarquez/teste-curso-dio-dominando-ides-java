07dbd123ad852a23b0134d8eb0c241016dda42ea
RayneMarquez <rmpiedade@hotmail.com>
2022-03-05 18:53:54 -0300

    First Commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		
+		Gato gato = new Gato();
+		Livro livro= new Livro();
+		System.out.println(gato);
+		System.out.println(livro);
+		
+	}
+
+}
+
+class Livro{
+	public String livro;
+	public Integer numPaginas;
+}
